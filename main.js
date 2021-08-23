eeec75b565fa7bb4e28a9974a6c93e6849af90b9
tavolafourcade <tavolafourcade@gmail.com>
2021-08-23 15:17:22 -0500

    Getting Bingo unique values from 1 to 75

--- main.js
+++ main.js
@@ -10,10 +10,29 @@ where the winner will shout out "Bingo!" in order to let the caller and the room
 
 /* Calling Bingo Numbers */
 
-/* Calling a number between 1 and 75 inclusive */
-function call_number(){
-  return (Math.floor(Math.random()*75)+1)
+function checker(){
+  let values = [1,2,3,4,5,6,7,8,9,10,
+                11,12,13,14,15,16,17,18,19,20,
+                21,22,23,24,25,26,27,28,29,30,
+                31,32,33,34,35,36,37,38,39,40,
+                41,42,43,44,45,46,47,48,49,50,
+                51,52,53,54,55,56,57,58,59,60,
+                61,62,63,64,65,66,67,68,69,70,
+                71,72,73,74,75],
+    list = [],
+    i = values.length,
+    j = 0;
+
+  /* Calling a number between 1 and 75 inclusive */
+  while (i--) {
+      j = Math.floor(Math.random() * (i+1));
+      list.push(values[j]);
+      values.splice(j,1);
+  }
+  return list;
+
 }
 
-random_number = call_number();
-console.log(random_number);
+test = checker();
+console.log(test);
+
